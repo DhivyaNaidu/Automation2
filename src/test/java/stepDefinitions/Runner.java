bc85e9ffda9a5b69613da8900362c08def50eadc
DhivyaNaidu <43781093+DhivyaNaidu@users.noreply.github.com>
2018-10-10 00:36:52 +0530

    Update src/test/java/stepDefinitions/Runner.java

--- src/test/java/stepDefinitions/Runner.java
+++ src/test/java/stepDefinitions/Runner.java
@@ -6,7 +6,6 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)                                                                                     
-//@CucumberOptions(features = "src/test/resources/featureFiles/BookingFlow.feature", plugin = { "pretty", "json:target/cucumber.json","html:target/CucumberHTMLReport" }, monochrome = true, tags = { "@JBS_SC01_TC01" }) 
 @CucumberOptions(features = "src/test/resources/featureFiles/BookingFlow.feature", plugin = {"junit:target/cuc_report.xml"}, monochrome = true, tags = { "@JBS_SC01_TC01" }) 
 public class Runner {
 
